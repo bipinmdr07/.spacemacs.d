3486241b9ad500c7b59e4e6c1ca8d01577d34328
Bipin Manandhar <kubipin07@gmail.com>
2025-01-05 13:50:38 +0545

    Add hook to add branchname on commit by default.

--- +version-control-config.el
+++ +version-control-config.el
@@ -1,6 +1,11 @@
+;; -----------------------------------------------
 ;; Add current branch name to commit.
+;; -----------------------------------------------
 (defun add-branch-name-to-commit-message()
   (insert (concat (magit-get-current-branch) ": ")))
+
+(add-hook 'git-commit-mode-hook 'add-branch-name-to-commit-message)
+;; -----------------------------------------------
 
 ;; ------------------------------------------------
 ;; Accepting the both changes from the buffer A and B in ediff
@@ -13,5 +18,4 @@
                     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
 
 (add-hook 'ediff-keymap-setup-hook (lambda () (define-key ediff-mode-map "B" 'ediff-copy-both-to-C)))
-
 ;; ------------------------------------------------
